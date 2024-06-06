440009f212350dd43754e584099789496bc8c998
hossani <hamza.hossani.001@gmail.com>
2024-06-06 15:36:07 +0100

    modifie jeudi

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import {unsubscribeNewsletter,subscribeNewsletter}from '../controllers/userControllers';
+import authenticateUser from '../middlewares/authMiddlewares'
 
 const userRouter = express.Router();
 // Routes for user
-userRouter.post('/newsletter/unsubscribe', unsubscribeNewsletter);
-userRouter.post('/newsletter/subscribe', subscribeNewsletter);
+userRouter.post('/newsletter/unsubscribe', authenticateUser,unsubscribeNewsletter);
+userRouter.post('/newsletter/subscribe',authenticateUser, subscribeNewsletter);
 
 export default userRouter;
